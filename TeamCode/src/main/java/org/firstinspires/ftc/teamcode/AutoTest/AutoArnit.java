196c5a9253a8619041dbc2fb215188ac76c5c8f4
FTC4924 <nrrobotics@gmail.com>
2023-01-20 16:26:09 -0500

    Merged the TeleopBase from the ftclib_overhaul branch. Adjusted gripper positions for the new gripper. Created a new autonomous program to deposit a cone on the high goal when the signal says blue.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoTest/AutoArnit.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoTest/AutoArnit.java
@@ -1,35 +1,41 @@
 package org.firstinspires.ftc.teamcode.AutoTest;
 
+import static org.firstinspires.ftc.teamcode.Constants.CHASSIS_RADIUS;
+
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 
 import org.firstinspires.ftc.teamcode.AllianceColor;
 import org.firstinspires.ftc.teamcode.AutoBase;
 import org.firstinspires.ftc.teamcode.Commands.ArmRotate;
 import org.firstinspires.ftc.teamcode.Commands.Command;
 import org.firstinspires.ftc.teamcode.Commands.DetectSignalSide;
+import org.firstinspires.ftc.teamcode.Commands.GripperGrab;
+import org.firstinspires.ftc.teamcode.Commands.LiftRaise;
 import org.firstinspires.ftc.teamcode.Commands.LoadSignalSide;
 import org.firstinspires.ftc.teamcode.Commands.Move;
 import org.firstinspires.ftc.teamcode.Commands.Pause;
+import org.firstinspires.ftc.teamcode.Commands.Turn;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 
-import static org.firstinspires.ftc.teamcode.Constants.CHASSIS_RADIUS;
-
-@Autonomous(name="AutoSignalPark")
-public class AutoSignalPark extends AutoBase {
+@Autonomous(name = "AutoArnit")
+public class AutoArnit extends AutoBase {
     protected AllianceColor getAllianceColor() {
         return AllianceColor.BLUE;
     }
+
     protected boolean exitOnLastCommand = true;
+
     protected ArrayList<Command> getCommands() {
         return new ArrayList<Command>(
                 Arrays.asList(
-                       // new ArmRotate(0.75), Gabe said delete it, Ethan disagrees
+
                         new Pause(0.125),
                         new DetectSignalSide(),
-                        new Move(2.9 - CHASSIS_RADIUS,0.4),
+                        new Move(2.9 - CHASSIS_RADIUS, 0.4),
                         new Pause(0.25),
                         new LoadSignalSide(
                                 new ArrayList<>(
@@ -49,7 +55,25 @@ public class AutoSignalPark extends AutoBase {
                                 )
                         ),
                         new Pause(0.25),
-                        new Move(1,0.4),
+                        new Move(1, 0.4),
+                        new LoadSignalSide(
+                                new ArrayList<>(),
+                                new ArrayList<>(
+                                        Arrays.asList(
+                                                new ArmRotate(.80),
+                                                new LiftRaise(.85),
+                                                new Turn(-30),
+                                                new LiftRaise(0.80),
+                                                new GripperGrab(1.0),
+                                                new Pause(1),
+                                                new ArmRotate(.65),
+                                                new GripperGrab(0),
+                                                new ArmRotate(0),
+                                                new LiftRaise(0)
+                                        )
+                                ),
+                                new ArrayList<>()
+                        ),
                         new Pause(0.25)
                 )
         );
